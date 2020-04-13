e66f0f8868d04d8df6f47179898e7583121585e2
Steve Lhomme <slhomme@matroska.org>
2020-04-13 12:23:09 +0200

    coremake: remove ps2 support

--- corec/tools/coremake/config_helper.h
+++ corec/tools/coremake/config_helper.h
@@ -63,7 +63,7 @@
 
 /* forbid some defines */
 
-#if defined(TARGET_LINUX) || defined(TARGET_PS2SDK)
+#if defined(TARGET_LINUX)
 #undef COREMAKE_UNICODE /* platforms where Unicode is handled via UTF-8 strings */
 #endif
 
