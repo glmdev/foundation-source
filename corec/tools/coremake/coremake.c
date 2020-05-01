5914b7b1d740aab4b809109ba57dd4de3e80568e
Steve Lhomme <slhomme@matroska.org>
2020-05-01 14:24:54 +0200

    corec: set the PROJECT_VENDOR directly in each project

--- corec/tools/coremake/coremake.c
+++ corec/tools/coremake/coremake.c
@@ -2715,9 +2715,6 @@ static void preprocess_stdafx(item* p,int lib, const char *pro_root, const char 
 				    {
 					    fprintf(f,"\nvoid ProjectSettings(nodecontext* p)\n");
 					    fprintf(f,"{\n");
-					    fprintf(f,"#ifdef PROJECT_VENDOR\n");
-					    fprintf(f,"    Node_SetData((node*)p,NODECONTEXT_PROJECT_VENDOR,TYPE_STRING,PROJECT_VENDOR);\n");
-					    fprintf(f,"#endif\n");
 					    fprintf(f,"#ifdef PROJECT_HELP\n");
 					    fprintf(f,"    Node_SetData((node*)p,NODECONTEXT_PROJECT_HELP,TYPE_STRING,PROJECT_HELP);\n");
 					    fprintf(f,"#endif\n");
--- mkparts/mkparts.c
+++ mkparts/mkparts.c
@@ -164,6 +164,7 @@ int main(int argc, const char *argv[])
     // Core-C init phase
     ParserContext_Init(&p,NULL,NULL,NULL);
     Node_SetData(&p.Base.Base.Base,NODECONTEXT_PROJECT_NAME,TYPE_STRING,"mkparts");
+    Node_SetData(&p.Base.Base.Base,NODECONTEXT_PROJECT_VENDOR,TYPE_STRING,"Matroska");
     ProjectSettings(&p.Base);
 
     // EBML & Matroska Init
